c21f4563cc420210b6e89e1a5ed835f70992bfb5
Yuriy Litvinov <bmwproexpert@gmail.com>
2025-05-06 06:43:16 +0300

    Fixed public/private modificators

--- IType.h
+++ IType.h
@@ -3,7 +3,7 @@
 namespace Type{
     template <typename T>
     class IType{
-        private:
+        public:
             virtual void spinToRight() = 0;
             virtual void spinToLeft() = 0;
             virtual T read() = 0;
--- IntType.h
+++ IntType.h
@@ -3,13 +3,13 @@
 namespace Type{
     class IntType : public IType<int>{
         private:
+            int pos;
+            std::string path;
+        public:
             void spinToRight() override;
             void spinToLeft() override;
             int read() override;
             void write(int value) override;
-        public:
-            int pos;
-            std::string path;
             IntType(const std::string& path): path(path), pos(0){};
     };
 }
